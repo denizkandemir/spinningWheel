5a94859d00ee21321d545e999275ba3a7098d089
denizkandemir <denizkandemir137@gmail.com>
2025-10-28 17:58:51 +0100

    musics changed

--- frontend/src/objects/songs.jsx
+++ frontend/src/objects/songs.jsx
@@ -1,6 +1,6 @@
 export const songs = [
     { title: "Symphony of Destruction", src: "/music/symphony.mp3" , id: 1},
-    { title: "Fade to Black", src: "/music/fadeToBlack.mp3" , id: 2},
-    { title: "One", src: "/music/one.mp3" , id: 3},
-    { title: "Ride the Lightning", src: "/music/rideTheLightning.mp3" , id: 4   },
+    { title: "Losing My Religion", src: "/music/losingMyReligion.mp3" , id: 2},
+    { title: "In My Darkest Hour", src: "/music/darkest.mp3" , id: 3},
+    { title: "Ride the Lightning (Live in Helsinki)", src: "/music/lightning.mp3" , id: 4   },
 ];
